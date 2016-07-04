3384f5472798fc913b83a479e36db8f02384eebb
Pancake <1479833945@qq.com>
2016-07-04 15:35:46 +0800

    first

---
+++ showPic.js
@@ -0,0 +1,28 @@
+function showPic(whichpic){
+    var source=whichpic.getAttribute("href");
+    var placeholder=document.getElementById("placeholder");
+    placeholder.setAttribute("src",source);
+    var text=whichpic.getAttribute("title");
+    var description=document.getElementById("description");
+    description.firstChild.nodeValue=text;
+}
+//function countBodyChildren(){
+//    var body_element=document.getElementsByTagName("body")[0];
+//    alert(body_element.childNodes[0].nodeValue);
+//}
+function prepareGallery() {
+    if (!document.getElementById)return false;
+    if (!document.getElementsByTagName)return false;
+    if (!document.getElementById("imagegallery"))return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            showPic(this);
+            return false;
+        }
+
+    }
+}
+window.onload = prepareGallery;
+
